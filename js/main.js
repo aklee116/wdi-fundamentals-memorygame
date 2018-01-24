9bacd1fc8e266c2d0467feb6e1c09c896e416ed0
Angela Lee <akang116@yahoo.com>
2018-01-23 21:32:53 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,19 +2,24 @@
 
   var cards = ["queen", "queen", "king", "king"];
   var cardsInPlay = [];
-  var cardOne = cards[2];
-  cardsInPlay.push(cardOne);
-  console.log("User flipped queen");
-  var cardTwo = cards[3];
-  cardsInPlay.push(cardTwo);
-  console.log("User flipped king");
+
+var checkForMatch = function (){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry try again.");
+  }
+}
+
+var flipCard = function (cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+
   if (cardsInPlay.length === 2) {
-
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-
-      alert("You found a match!");
-    }
-    else {
-      alert("Sorry try again.");
+    checkForMatch();
     }
   }
+
+flipCard (0);
+flipCard (1);
